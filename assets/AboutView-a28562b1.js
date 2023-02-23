293071b5eea1a6093dd81d92c71e24328e29afa7
aaron00366 <65890513+aaron00366@users.noreply.github.com>
2023-02-24 00:03:26 +0800

    deploy

---
+++ assets/AboutView-a28562b1.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index-8af8bdf4.js";const t={},c={class:"home"};function a(n,r,_,i,p,d){return o(),s("div",c," this is about pages ")}const u=e(t,[["render",a]]);export{u as default};
